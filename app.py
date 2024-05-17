b7afc509eb495c9c0ce9b4a885284841e7876b0e
ibrahimwichka <113219188+ibrahimwichka@users.noreply.github.com>
2024-05-17 11:34:22 -0400

    initialize app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, url_for, request
+from flask_material import Material
+
+app = Flask(__name__)
+Material(app)
+
+@app.route('/')
+def index():
+    return "betBCA App"
