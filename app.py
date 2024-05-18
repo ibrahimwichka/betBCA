8b7ffc300f3805fe450b660ec200bb2794a7c502
ibrahimwichka <113219188+ibrahimwichka@users.noreply.github.com>
2024-05-17 20:43:35 -0400

    created requirements.txt

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request
 from flask_material import Material
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 Material(app)
@@ -7,3 +8,7 @@ Material(app)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
