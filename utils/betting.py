e7de1a8d2b6488ad7835c2acf1d2c45a292aa606
ibrahimwichka <113219188+ibrahimwichka@users.noreply.github.com>
2024-06-09 21:46:14 -0400

    new commit

--- utils/betting.py
+++ utils/betting.py
@@ -1,11 +1,9 @@
-# converts the chance of a team winning to a betting odds: ex: +300
-# does not artificially inflate the percentages
 def set_odds(percent_chance):
     if (percent_chance < 50):
         return (1/(percent_chance/100) * 100)
     return -(1/(percent_chance/100) * 100)
 
-# artificially set the sum of the probabilities to a probability higher than 100
+
 def inflate_probability(chance, goal_percent):
     chance *= goal_percent
     return chance
